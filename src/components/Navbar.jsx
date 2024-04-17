8c16c4c57f77d25ddcc12132321d1680ed4cc0e6
Feryal Doubali <feryalhalima@gmail.com>
2024-04-17 18:51:36 +0200

    starting frontend

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+// Navbar.js
+
+function Navbar() {
+  return (
+    <nav className="bg-yellow-600">
+      <div className="container mx-auto flex justify-between items-center justify-end">
+        <div className="flex items-center">
+          <img src="./assets/logo.png" alt="Logo" className="h-16 w-auto" />
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
