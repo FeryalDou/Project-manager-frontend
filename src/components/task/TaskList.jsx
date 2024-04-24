63dfecbd1a51aa09cdd9c5f4b9ea1efe196ce958
Feryal Doubali <feryalhalima@gmail.com>
2024-04-24 09:24:08 +0200

    done

--- src/components/task/TaskList.jsx
+++ src/components/task/TaskList.jsx
@@ -3,7 +3,7 @@ import Task from "./Task";
 
 const TaskList = ({ tasks }) => {
   return (
-    <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+    <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-4">
       {tasks && tasks.map((task) => <Task key={task._id} task={task} />)}
     </div>
   );
--- src/pages/ProjectDetailPage.jsx
+++ src/pages/ProjectDetailPage.jsx
@@ -1,76 +1,138 @@
 import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import projectManagerApi from "../service/myApi.js";
-import TaskForm from "../components/task/TaskForm.jsx";
-import TaskList from "../components/task/TaskList.jsx";
 
-function TaskPage() {
-  const [tasks, setTasks] = useState([]);
+function ProjectDetailsPage() {
+  const { projectId } = useParams();
+  const [project, setProject] = useState(null);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedProject, setEditedProject] = useState(null);
 
   useEffect(() => {
-    const fetchTasks = async () => {
+    const fetchProjectDetails = async () => {
       try {
-        const response = await projectManagerApi.get("/tasks");
-        setTasks(response.data);
+        const response = await projectManagerApi.get(`/projects/${projectId}`);
+        setProject(response.data);
+        setEditedProject(response.data);
       } catch (error) {
-        console.error("Error fetching tasks", error);
+        console.error("Error fetching project details", error);
       }
     };
-    fetchTasks();
-  }, []);
+    fetchProjectDetails();
+  }, [projectId]);
 
-  const handleAddTask = async (formData) => {
+  const handleDeleteProject = async () => {
     try {
-      await projectManagerApi.post("/tasks", formData);
-      const response = await projectManagerApi.get("/tasks");
-      setTasks(response.data);
+      await projectManagerApi.delete(`/projects/${projectId}`);
     } catch (error) {
-      console.error("Error adding task", error);
+      console.error("Error deleting project", error);
     }
   };
 
-  const handleUpdateTask = async (taskId, formData) => {
+  const handleUpdateProject = async () => {
     try {
-      await projectManagerApi.put(`/tasks/${taskId}`, formData);
-      const response = await projectManagerApi.get("/tasks");
-      setTasks(response.data);
+      await projectManagerApi.put(`/projects/${projectId}`, editedProject);
+      setIsEditing(false);
+      setProject(editedProject);
     } catch (error) {
-      console.error("Error updating task", error);
+      console.error("Error updating project", error);
     }
   };
 
-  const handleDeleteTask = async (taskId) => {
-    try {
-      await projectManagerApi.delete(`/tasks/${taskId}`);
-      const updatedTasks = tasks.filter((task) => task._id !== taskId);
-      setTasks(updatedTasks);
-    } catch (error) {
-      console.error("Error deleting task", error);
-    }
+  const handleEditClick = () => {
+    setIsEditing(true);
   };
 
-  return (
-    <div className="container mx-auto px-4 py-8">
-      <h1 className="text-center text-3xl font-bold mb-8">Tasks</h1>
-      <div className="flex justify-center">
-        <div className="w-full md:w-1/2 pr-4">
-          <TaskForm onAddTask={handleAddTask} />
-        </div>
-        <div className="w-full md:w-1/2 pl-4">
-          {tasks.length === 0 ? (
-            <p className="text-gray-600">No tasks available.</p>
-          ) : (
-            <div className="border border-gray-300 rounded-lg p-2">
-              <TaskList
-                tasks={tasks}
-                onUpdateTask={handleUpdateTask}
-                onDeleteTask={handleDeleteTask}
-              />
-            </div>
-          )}
-        </div>
+  const handleCancelEdit = () => {
+    setIsEditing(false);
+    setEditedProject(project);
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setEditedProject((prevProject) => ({
+      ...prevProject,
+      [name]: value,
+    }));
+  };
+
+  if (!projectId) {
+    return <div className="text-center mt-8">Loading...</div>;
+  }
+
+  return project ? (
+    <div className="container mx-auto px-4 py-8 border border-gray-300 rounded-lg max-w-lg mx-auto my-8">
+      <h1 className="text-3xl font-bold mb-4">
+        {isEditing ? (
+          <input
+            type="text"
+            name="name"
+            value={editedProject.name}
+            onChange={handleChange}
+            className="w-full py-2 px-3 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+          />
+        ) : (
+          project.name
+        )}
+      </h1>
+      <p className="text-gray-700 mb-4">
+        {isEditing ? (
+          <textarea
+            name="description"
+            value={editedProject.description}
+            onChange={handleChange}
+            className="w-full py-2 px-3 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+          />
+        ) : (
+          project.description
+        )}
+      </p>
+      <div className="flex justify-between">
+        {isEditing ? (
+          <>
+            <button
+              onClick={handleUpdateProject}
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mr-4"
+            >
+              Save
+            </button>
+            <button
+              onClick={handleCancelEdit}
+              className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            >
+              Cancel
+            </button>
+          </>
+        ) : (
+          <button
+            onClick={handleEditClick}
+            className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mr-4"
+          >
+            Edit
+          </button>
+        )}
+        <button
+          onClick={handleDeleteProject}
+          className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        >
+          Delete
+        </button>
+      </div>
+      <div>
+        <h2 className="text-2xl font-bold my-4">Tasks</h2>
+        <ul>
+          {project.tasks &&
+            project.tasks.map((task) => (
+              <li key={task._id}>
+                <p>{task.title}</p>
+                <p>{task.description}</p>
+                <p>{task.endDate}</p>
+              </li>
+            ))}
+        </ul>
       </div>
     </div>
-  );
+  ) : null;
 }
 
-export default TaskPage;
+export default ProjectDetailsPage;
